edaf21a136030e05941b42908f97b3a5c2db1c85
Bet Hm <bbhm.40@gmail.com>
2026-09-09 22:28:41 -0300

    cria dados iniciais dos produtos

---
+++ src/data/produtos.js
@@ -0,0 +1,26 @@
+export const produtos = [
+  {
+    id: 1,
+    nome: 'Fone de Ouvido Bluetooth',
+    preco: 199.90,
+    quantidade: 1
+  },
+  {
+    id: 2,
+    nome: 'Mouse Sem Fio Ergonômico',
+    preco: 89.50,
+    quantidade: 2
+  },
+  {
+    id: 3,
+    nome: 'Teclado Mecânico Compacto',
+    preco: 349.00,
+    quantidade: 1
+  },
+  {
+    id: 4,
+    nome: 'Suporte para Notebook',
+    preco: 79.90,
+    quantidade: 1
+  }
+];
